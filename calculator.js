b25953fce4853878a0a52c77aff76aaf5febd720
jmschonfeld <jeremyschonfeld@gmail.com>
2017-02-06 09:30:11 -0500

    Setup script

--- calculator.js
+++ calculator.js
@@ -1,1 +1,31 @@
 /** PHYS 2211 Grade Calculator Script **/
+
+$("#data-results").hide();
+
+$(".submit").click(submitForm());
+$(".reset").click(resetForm());
+
+
+function submitForm() {
+	var values = {};
+	$("input").each(function() {
+		values[$(this).attr("name")] = $(this).val();
+		$(this).attr("disabled", "true");
+	});
+
+
+	$("#data-results").show();
+	$("#data-entry").hide();
+	$(".submit").hide();
+
+}
+
+function resetForm() {
+	$("input").each(function() {
+		$(this).val('');
+		$(this).attr("disabled", "false");
+	});
+	$("#data-results").hide();
+	$("#data-entry").show();
+	$(".submit").show();
+}
