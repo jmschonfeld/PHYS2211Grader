7d28169540c538ecc5d31c56f8dcac85ca4408e2
jmschonfeld <jeremyschonfeld@gmail.com>
2017-02-07 13:18:48 -0500

    Add in prediction calculation for remaining tests

--- calculator.js
+++ calculator.js
@@ -21,6 +21,7 @@ $("input[type=text][aria-label]").on('input propertychange paste', function(even
 });
 
 var submitForm = function(e) {
+	e.preventDefault();
 	var values = {};
 	var go = true;
 	var totCheck = 0.0;
@@ -54,15 +55,30 @@ var submitForm = function(e) {
 	if (!go) {
 		isResetBack = true;
 		resetForm();
-		e.preventDefault();
 		return false;
 	}
 	values['totCheck'] = totCheck;
 	$(".reset").addClass("disabled");
 	isResetBack = true;
 
-	var grades = calcResults(values);
-	displayResults(grades);
+	if (isPrediction(values)) {
+
+		if (!isValidPrediction(values)) {
+			console.log("invalid prediction (blank fields)");
+			alert("In valid test score, please fill in ALL scores that you have so far and leave future tests blank");
+			isResetBack = true;
+			resetForm();
+			return false;
+		}
+		var predictions = predict(values);
+		displayPredictions(predictions);
+
+	} else {
+
+		var grades = calcResults(values);
+		displayResults(grades);
+
+	}
 
 	$("#data-entry").fadeOut(1000, function() {
 		$("#data-results").fadeIn(1000, function() {
@@ -95,6 +111,68 @@ var resetForm = function() {
 
 $(".reset").click(resetForm);
 $("#gradeform").submit(submitForm);
+
+function isPrediction(values) {
+	return !values['testA'] || !values['testB'] || !values['testC'] || !values['testD'] || !values['final'];
+}
+
+function isValidPrediction(values) {
+	var check = ['testA', 'testB', 'testC', 'testD', 'final'];
+	var foundBlank = false;
+	for (i = 0; i < check.length; i++) {
+		if (foundBlank) {
+			if (check[i]) {
+				return false;
+			}
+		} else {
+			if (!check[i]) {
+				foundBlank = true;
+			}
+		}
+	}
+	return true;
+}
+
+function predict(values) {
+	console.log("Predicting grades with values " + JSON.stringify(values));
+
+	// -- unchanged --
+	var bucketPossible = (parseFloat(values['clickers']) * 3.0 * values['clickers-check'] + parseFloat(values['homework']) * 5.0 * values['homework-check'] +
+									parseFloat(values['optional']) * 3.0  * values['optional-check'] + parseFloat(values['wiki']) * 2.0 * values['wiki-check'] +
+									parseFloat(values['reading']) * values['reading-check'] + parseFloat(values['lectures']) * values['lectures-check']) / values['totCheck'];
+	var extraCredit = Math.max(0, Math.min(5, bucketPossible * values['totCheck'] / 100.0 - parseFloat(values['homework']) * 5.0 / 100) + parseFloat(values['homework']) * 5.0 / 100.0 - 10.0) / 2.0 * 100.0;
+	var bucket = Math.min(10, (bucketPossible * values['totCheck']) / 100.0) / 0.1;
+	// -- unchanged --
+
+	var curTestAvg = predTestAvg(values['test1'], values['test2'], values['test3'], values['test4']);
+
+	var needed = [
+		testAvgNeeded(90.0, values, extraCredit, bucket, curTestAvg),
+		testAvgNeeded(80.0, values, extraCredit, bucket, curTestAvg),
+		testAvgNeeded(70.0, values, extraCredit, bucket, curTestAvg),
+		testAvgNeeded(60.0, values, extraCredit, bucket, curTestAvg)
+	];
+
+	var obj = {
+		"bucketPossible": bucketPossible,
+		"extraCredit": extraCredit,
+		"bucket": bucket,
+		"curAvg": curTestAvg,
+		"needed": needed
+	};
+	console.log("RESULTS: " + JSON.stringify(obj));
+	return obj;
+
+}
+
+function testAvgNeeded(goal, values, extraCredit, bucket, curAvg) {
+	console.log("Getting average needed for goal " + goal);
+	var a =  (goal * 100.0 - extraCredit * 2.0 - bucket * 10.0) / 90.0 * 0.9 * 100.0  - parseFloat(values['labA']) * 5.0 - parseFloat(values['labB']) * 10.0 - parseFloat(values['labC']) * 5.0;
+	//assuming a 90 on the final:
+	var b = (a - 90.0 * 25.0) / 45.0;
+	var ret = (b * 4.0 - curAvg.sum) / (4.0 - curAvg.tot);
+	return ret;
+}
 
 function calcResults(values) {
 	console.log("Calculating results with values " + JSON.stringify(values));
@@ -138,9 +216,36 @@ function displayResults(grades) {
 	$("#res-display").html(html);
 }
 
+function displayPredictions(predict) {
+	var html = "<strong>Current Bucket Possible:</strong> " + rnd(predict['bucketPossible']);
+	html += "<br><strong>Current Extra Credit:</strong> " + rnd(predict['extraCredit']);
+	html += "<br><strong>Current Bucket Points:</strong> " + rnd(predict['bucket']);
+	html += "<br><strong>Current Test Average (without weights):</strong> " + rnd(predict.curAvg.sum / predict.curAvg.tot);
+	html += "<br><br><h3>Test Average Goals:</h3><br><small>These are the total averages of the <i>remaining</i> tests that you need to reach in order to achieve each grade level for the class assuming a 90% on the final</small><br>";
+	html += "<br><strong>90% (A):</strong> " + rnd(predict['needed'][0]) + "%";
+	html += "<br><strong>80% (B):</strong> " + rnd(predict['needed'][1]) + "%";
+	html += "<br><strong>70% (C):</strong> " + rnd(predict['needed'][2]) + "%";
+	html += "<br><strong>60% (D):</strong> " + rnd(predict['needed'][3]) + "%";
+	$("#res-display").html(html);
+}
+
 
 function sortNumber(a, b) {
 	return a - b;
+}
+
+function predTestAvg(testA, testB, testC, testD) {
+	var sum = 0.0;
+	var tot = 0.0;
+	var tests = [testA, testB, testC, testD];
+	console.log("predicting tests " + JSON.stringify(tests));
+	for (i = 0; i < 4; i++) {
+		if (tests[i]) {
+			sum += parseFloat(tests[i]);
+			tot++;
+		}
+	}
+	return {"sum": sum, "tot": tot};
 }
 
 function calcTestAverage(testA, testB, testC, testD) {
